07b887e51b01c010620d304189ee1f56613a26e4
davidseruga <56600501+davidseruga@users.noreply.github.com>
2022-08-23 14:39:12 +0200

    Teden3 semi done

--- vaja/Teden03/dn03_testi/naloga1/inverz.c
+++ vaja/Teden03/dn03_testi/naloga1/inverz.c
@@ -4,9 +4,24 @@
 #include "inverz.h"
 
 long inverz(long x, long a, long b) {
+    long sredina;
+    long fn;
+    do
+    {
+        sredina = (b - a) / 2 + a;
+        fn = f(sredina);
+        if (fn > x)
+            b = sredina - 1;
+        else if (fn < x)
+            a = sredina + 1;
+    } while (x != fn && a <= b);
+    
+    return sredina;
+    /*
     for (long i = a; i <= b; i++) {
         if (x == f(i))
             return i;
     }
     return -1;
+    */
 }
